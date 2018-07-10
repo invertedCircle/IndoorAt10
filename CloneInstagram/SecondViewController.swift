75a829d7056f40d58fff55c95544900f59b7df62
DONALD innocent ike <yungdinno@gmail.com>
2018-07-10 17:37:28 -0400

    Initial Commit

---
+++ CloneInstagram/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CloneInstagram
+//
+//  Created by Donald Mactavish on 10/07/2018.
+//  Copyright © 2018 Inverted Circle. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
